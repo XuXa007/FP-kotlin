a36c869f3a7ae3761e6f10b7b3cce967c22ddd03
XuXa007 <housegregogri@gmail.com>
2024-02-24 02:48:49 +0300

    init and add Enter_1 and add Var_2

---
+++ src/main/kotlin/Enter_1.kt
@@ -0,0 +1,5 @@
+fun main() {
+    println("Ivan")
+    println(35)
+    println(true)
+}
---
+++ src/main/kotlin/Var_2.kt
@@ -0,0 +1,9 @@
+var a: Int = 5
+var b: String = "F"
+var c: String = "Привет"
+val d: Int = 65
+
+fun main() {
+    println(d)
+    println(c)
+}
