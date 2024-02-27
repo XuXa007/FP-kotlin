40dc04e48b8ed80d5117286fcf6aa69d1f0b1693
XuXa007 <housegregogri@gmail.com>
2024-02-27 21:17:15 +0300

    do 2-5 task

--- src/main/kotlin/conditional_statements/2_Simple.kt
+++ src/main/kotlin/conditional_statements/2_Simple.kt
@@ -7,7 +7,10 @@ fun main() {
         println("Вам уже все можно")
     } else if (age == 18) {
         println("Ура, Вам 18 лет!")
+    } else if (age <= 0 ) {
+        println("Ошибка")
     } else {
         println("Вы еще слишком молоды")
+
     }
 }
--- src/main/kotlin/conditional_statements/5_Average.kt
+++ src/main/kotlin/conditional_statements/5_Average.kt
@@ -3,9 +3,9 @@ package conditional_statements
 
 fun main() {
     println("Введите 3 числа")
-    val n1 = readln().toInt() //2
-    val n2 = readln().toInt() // 3
-    val n3 = readln().toInt() // 1
+    val n1 = readln().toInt()
+    val n2 = readln().toInt()
+    val n3 = readln().toInt()
 
     when {
         (n2 > n3 && n2 < n1 || n2 > n1 && n2 < n3) -> println(n2)
@@ -14,13 +14,19 @@ fun main() {
 
         else -> {
             when {
-                (n1<n2 && n1<n3) -> println(n1)
-                (n2<n3 && n2<n1) -> println(n2)
-                (n3<n1 && n3<n2) -> println(n3)
+                (n1 < n2 && n1 < n3) -> println(n1)
+                (n2 < n3 && n2 < n1) -> println(n2)
+                (n3 < n1 && n3 < n2) -> println(n3)
+                (n1 == n2 && n2 == n3) -> println("они равны")
+                else -> {
+                    when {
+                        (n1 == n2) -> println(n1)
+                        (n2 == n3) -> println(n2)
+                        (n1 == n3) -> println(n3)
+                    }
+                }
             }
         }
-
     }
-
 }
 
