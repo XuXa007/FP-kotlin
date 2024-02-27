c25f95805ffabd89dc2ebba1547d30700efbc84e
XuXa007 <housegregogri@gmail.com>
2024-02-27 17:54:27 +0300

    add fix to 2_5 - average

--- src/main/kotlin/conditional_statements/5_Average.kt
+++ src/main/kotlin/conditional_statements/5_Average.kt
@@ -3,16 +3,24 @@ package conditional_statements
 
 fun main() {
     println("Введите 3 числа")
-    val n1 = readln()?.toInt()
-    val n2 = readln()?.toInt()
-    val n3 = readln()?.toInt()
+    val n1 = readln().toInt() //2
+    val n2 = readln().toInt() // 3
+    val n3 = readln().toInt() // 1
 
-    if (n1!=null && n2!=null && n3!=null) {
-        val res = findAverage(n1, n2, n3)
-        println("Averange: $res")
+    when {
+        (n2 > n3 && n2 < n1 || n2 > n1 && n2 < n3) -> println(n2)
+        (n1 > n3 && n1 < n2 || n1 > n2 && n1 < n3) -> println(n1)
+        (n3 > n2 && n3 < n1 || n3 > n1 && n3 < n2) -> println(n3)
+
+        else -> {
+            when {
+                (n1<n2 && n1<n3) -> println(n1)
+                (n2<n3 && n2<n1) -> println(n2)
+                (n3<n1 && n3<n2) -> println(n3)
+            }
+        }
+
     }
+
 }
 
-fun findAverage(n1: Int, n2: Int, n3: Int) {
-    println("Error")
-}
