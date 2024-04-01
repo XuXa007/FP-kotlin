8ca9da5413a681e95aee662400765bf835251051
Ксюша Лягашкина <xl@192.168.1.3>
2024-04-02 02:52:36 +0300

    finish 4-5

--- src/main/kotlin/4_arrays_of_data/5_sort_arr.kt
+++ src/main/kotlin/4_arrays_of_data/5_sort_arr.kt
@@ -1,6 +1,12 @@
 package `4_arrays_of_data`
 
+import java.util.*
+
 fun main() {
+    val scanner = Scanner(System.`in`)
+
+//    val input = scanner.nextLine()
+//    val arr1 = input.split(", ").map { it.toInt() }
     val arr1 = listOf(24, -63, 67, -12, 88, 94, -28, 82, 0, 53)
-    println(arr1.sorted())
+    for (i in arr1.sorted()) print("$i ")
 }
