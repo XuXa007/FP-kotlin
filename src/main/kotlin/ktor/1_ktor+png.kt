31d27e7974bf9e30df2a8506fce090a47558ffa8
Ксюша Лягашкина <xl@MacBook-Pro-Ksusa.local>
2024-05-22 04:40:49 +0300

    add text about Dispatchers.Default

--- src/main/kotlin/ktor/1_ktor+png.kt
+++ src/main/kotlin/ktor/1_ktor+png.kt
@@ -9,14 +9,6 @@ import java.io.File
 
 fun main() {
     val client = HttpClient(CIO)
-//    val urls = listOf(
-//        "https://klkfavorit.ru/wp-content/uploads/f/7/e/f7edfaf9261573a3557c06c37d1da21b.jpeg",
-//        "https://laplaya-rus.ru/wp-content/uploads/a/4/0/a40ce98be1ba83423a28acf8b5c2d0b5.jpeg",
-//        "https://ferret-pet.ru/wp-content/uploads/6/b/5/6b5f022448da0963245cbf3e0878dd0d.jpeg",
-//        "https://flomaster.top/uploads/posts/2023-01/1673477911_flomaster-club-p-syurrealizm-risunki-vkontakte-8.jpg",
-//        "https://klike.net/uploads/posts/2023-02/1675234996_3-35.jpg"
-//    )
-
     val urls = List(100) { "https://picsum.photos/200/300?random=${it + 1}" }
     val downloadsDir = File("downloads_1")
     if (!downloadsDir.exists()) {
@@ -25,12 +17,35 @@ fun main() {
         downloadsDir.listFiles()?.forEach { it.delete() }
     }
 
+    /*
+    runBlocking - для запуска корутин в основном потоке, блокируя его до завершения всех задач внутри этого блока.
+    withContext и Dispatchers.Default - для переключения контекста выполнения на Dispatchers.Default, что
+    позволяет выполнять задачи в фоновом потоке.
+    async - создает асинхронную задачу, которая возвращает Deferred
+    await - используется для получения результата выполнения этой задачи
+    */
+
     runBlocking {
         val images = withContext(Dispatchers.Default) {
+            /*
+            отвечает за определение потока(ов), на котором будут выполняться корутины
+            Он использует общий пул потоков, который создается на основе количества доступных процессорных ядер
+
+            Dispatchers.IO: Оптимизирован для задач ввода-вывода, таких как чтение и запись файлов,
+            Dispatchers.Main: Используется для выполнения задач в главном (UI) потоке
+            Dispatchers.Unconfined: Запускает корутину в текущем потоке, но только до первой приостановки
+            */
             urls.map { url ->
+                /*
+                Для каждого URL создается асинхронная задача, которая выполняет HTTP GET
+                запрос и возвращает тело ответа в виде массива байтов
+                */
                 async {
                     client.get(url).body<ByteArray>()
                 }
+            /*
+            ожидается завершение всех асинхронных задач и получение результатов
+            */
             }.map { it.await() }
         }
         images.forEachIndexed { index, image ->
